50eb5c4c0f24cd15a64cbd7369996fb3ae7e029a
masoume <masoumevsl198@gmail.com>
2020-04-08 23:42:19 +0430

    style
    add style in file App.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import { Row } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>hello</h1>
+    <div className="container">
+      <Row className="purchase-card">
+        <h1>Hellow</h1>
+      </Row>
     </div>
   );
 }
