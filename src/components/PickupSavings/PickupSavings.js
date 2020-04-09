7ecbf061ba288dcfcef73f9e52653a86104ee74c
masoume <masoumevsl198@gmail.com>
2020-04-09 14:35:24 +0430

    add
    new folder PickupSaving and new file PickupSaving

---
+++ src/components/PickupSavings/PickupSavings.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+import { Row, Col, Tooltip, OverlayTrigger } from 'react-bootstrap';
+
+var styles = {
+    PickupSavings: {
+    textDecoration: 'none'
+    },
+    totalSaving:{
+        color:'red', 
+        fontWeight: 8000
+    }
+};
+
+
+export default class PickupSavings extends Component {
+    render() {
+        const Tooltip = (
+            <Tooltip id="Tooltip">
+                <p>Picking up your order in the store</p>
+            </Tooltip>
+        );
+        
+        return(
+            <Row className="show-grid row">
+                <Col md={6}>
+                    <OverlayTrigger placement="bottom">
+                        <div style={styles.PickupSavings}>pickup saving</div>
+                    </OverlayTrigger>
+                </Col>
+
+                <Col style={styles.totalSaving} md={6}>
+                    {`$${this.props.price}`}
+                </Col>
+
+            </Row>
+        )
+    }
+}
