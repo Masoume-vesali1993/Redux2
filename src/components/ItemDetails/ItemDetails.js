ea8db47120df245924c22273aa889565c4c82c28
masoume <masoumevsl198@gmail.com>
2020-04-11 16:27:19 +0430

    add
    part show img

--- src/components/ItemDetails/ItemDetails.js
+++ src/components/ItemDetails/ItemDetails.js
@@ -1,5 +1,5 @@
 import React, {Component } from 'react';
-import { Button, Collapse, Well, Media, Row, Col } from 'react-bootstrap';
+import { Button, Collapse, Card, Media, Row, Col } from 'react-bootstrap';
 
 
 export default class ItemDetails extends Component {
@@ -22,6 +22,20 @@ export default class ItemDetails extends Component {
                     { this.state.open === false ? `+` : `-`} 
                     
                 </Button>
+
+                <Collapse in={this.state.open}>
+                    <div>
+                        <Card>
+                            <Media>
+                                    <img width={100} 
+                                    height={100} 
+                                    alt="thumbnail"
+                                    src="https://i5.walmartimages.com/asr/013f4de0-db33-441b-983b-4c29aed6759f_1.1b0b38f8b97d204b442ea95bd922d0e0.jpeg?odnWidth=undefined&odnHeight=undefined&odnBg=ffffff"/
+                                    >
+                            </Media>
+                        </Card>
+                    </div>
+                </Collapse>
             </div>
         )
     }
