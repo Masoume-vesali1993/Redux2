b7ff20e542244af1922f8949db09fbc045610a64
masoume <masoumevsl198@gmail.com>
2020-04-11 18:19:24 +0430

    Redux
    add one part Redux

---
+++ src/components/PromoCode/PromoCode.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import {
+    Button,
+    Collapse,
+    Well,
+    Form,
+    Row,
+    Col,
+    FormGroup,
+    ControLLabel,
+    FormControl
+    } from 'react-bootstrap';
+// add part redux and global estate manager 
+    class PromoCodeDiscount extends Component {
+        constructor(props){
+            super(props);
+
+            this.state = {
+                
+            }
+        }
+    }
